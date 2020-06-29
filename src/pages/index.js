6a02b223f9277b47bf1e86d15bd0ddb66d6111cb
skitsanos <info@skitsanos.com>
2020-06-29 09:08:15 +0300

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,49 @@
+import {useSession} from '@/entities';
+import {Button, Col, Row, Steps, Typography} from 'antd';
+import React from 'react';
+
+const {Title} = Typography;
+
+import {remote} from 'electron';
+
+const {Step} = Steps;
+
+
+export default () =>
+{
+    const [session, {login, logout}] = useSession();
+
+    return <>
+        <Title>My app</Title>
+
+        <Row gutter={[10, 10]}>
+            <Col span={8}>Load mind map</Col>
+            <Col span={8}>Analysis</Col>
+            <Col span={8}>Result</Col>
+        </Row>
+
+        <Row gutter={[10, 10]}>
+            <Col span={8}>
+                <Button type={'primary'} onClick={() =>
+                {
+                    const {dialog} = remote;
+                    const selectedFiles = dialog.showOpenDialogSync({
+                        properties: ['openFile'],
+                        title: 'Open file'
+                    });
+                    if (selectedFiles.length > 0)
+                    {
+                        //ok
+                    }
+                }}>Select file</Button>
+            </Col>
+            <Col span={8}>
+                <Steps direction="vertical" current={1}>
+                    <Step title="Finished" description="This is a description."/>
+                    <Step title="In Progress" description="This is a description."/>
+                    <Step title="Waiting" description="This is a description."/>
+                </Steps></Col>
+            <Col span={8}></Col>
+        </Row>
+    </>;
+};
---
+++ src/theme.js
@@ -0,0 +1,5 @@
+export default {
+    'primary-color': '#884EA0',
+    'heading-color': '@primary-color',
+    'text-color': '#2d343a'
+};
